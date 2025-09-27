5e32a9e8d71f2cd55cdc416675bf6eaecf5662de
j0ruge <joruge@gmail.com>
2025-09-27 23:01:12 +0100

    07-Aliases

---
+++ math.exs
@@ -0,0 +1,6 @@
+defmodule MyModule.Math do
+    def sum(parameter1, parameter2) do
+        parameter1 + parameter2
+    end
+
+end
--- teste.exs
+++ teste.exs
@@ -1,8 +1,11 @@
-defmodule Hello do
+defmodule MyModule do
     import IO, only: [puts: 1]
     import Kernel, except: [inspect: 1]
+
+    alias MyModule.Math, as: MyMath
+    
     def gabiru do
-        inspect("Olá, GabirU!")
+        inspect(MyMath.sum(1,2))
     end
 
     def inspect(parametro) do
