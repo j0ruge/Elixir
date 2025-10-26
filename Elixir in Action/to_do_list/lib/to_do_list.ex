96e9d4cdacdb5ce0fc21c3213454621552020dfd
unknown <joruge@gmail.com>
2025-10-26 17:45:40 -0300

    To Do List

---
+++ Elixir in Action/to_do_list/lib/to_do_list.ex
@@ -0,0 +1,16 @@
+defmodule ToDoList do
+  @moduledoc """
+  Documentation for `ToDoList`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ToDoList.hello()
+      :world
+
+  """
+  def new,  do: Map.new
+end
