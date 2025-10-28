af8d8bfa6e1715bcde9017ab955853edf8afe1bb
JRC do Brasil Empreendimentos Eletrônicos LTDA <46424602+j0ruge@users.noreply.github.com>
2025-10-27 21:30:48 -0300

    4.1.3 Structuring data with maps

--- enum.exs
+++ enum.exs
@@ -1,16 +1,15 @@
 defmodule MyModule.Enum do
-   def first_element(list) do
-        if length(list) == 0 do
-            nil
-        else
-            hd(list)
-        end
+  def first_element(list) do
+    if length(list) == 0 do
+      nil
+    else
+      hd(list)
     end
+  end
 
-    def other_elements(list) do
-        unless length(list) == 0 do
-            tl(list)
-        end
+  def other_elements(list) do
+    unless length(list) == 0 do
+      tl(list)
     end
-   
+  end
 end
