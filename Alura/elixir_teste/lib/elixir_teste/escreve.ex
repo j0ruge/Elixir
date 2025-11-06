90a9b326bdf6614d553cd9b4e092a66b1f21af09
JorUge <joruge@gmail.com>
2025-11-06 07:55:39 -0300

    Adição de dependência no ElixirTeste

--- Alura/elixir_teste/lib/elixir_teste/escreve.ex
+++ Alura/elixir_teste/lib/elixir_teste/escreve.ex
@@ -1,4 +1,14 @@
 defmodule Mix.Tasks.Escreve do
+ use Mix.Task
+  @moduledoc """
+    Documentação do módulo
+
+    > `mix escreve`
+  
+  """
+  
+  @shortdoc "Escreve um número aleatório no arquivo.txt"
+  
   def run(_) do
     IO.puts("Executando código 'escreve' 🐀🪽")
     ElixirTeste.EscreveNumeroAleatorio.escreve()
