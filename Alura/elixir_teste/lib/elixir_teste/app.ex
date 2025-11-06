208341e4c12e9895f1d1767f3abb8f580d416771
JorUge <joruge@gmail.com>
2025-11-06 17:16:07 -0300

    Criando uma tarefa Alura

---
+++ Alura/elixir_teste/lib/elixir_teste/app.ex
@@ -0,0 +1,15 @@
+defmodule ElixirTest.Application do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # This is the new line
+      worker(ElixirTeste.Agendador, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: YourApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
