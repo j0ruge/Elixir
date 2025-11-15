ce6553b38028f70beb06dfbabdb6d309748c502d
j0ruge <joruge@gmail.com>
2025-11-15 09:49:59 -0300

    Reports Generator Paralel

--- projetos_elixir/reports_generator_paralel/lib/reports_generator.ex
+++ projetos_elixir/reports_generator_paralel/lib/reports_generator.ex
@@ -72,6 +72,7 @@ defmodule ReportsGenerator do
   def build_from_many(file_names) do
     file_names
     |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
     |> Enum.map(& &1)
   end
 
@@ -111,6 +112,20 @@ defmodule ReportsGenerator do
 
   def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
@@ -126,6 +141,8 @@ defmodule ReportsGenerator do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"users" => users, "foods" => foods}
 end
